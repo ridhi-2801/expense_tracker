4070e1c3a5c87b775092f7fa8ad18f938fb72c69
ridhi-2801 <ridhi2801@gmail.com>
2020-12-07 14:48:35 +0530

    final chnges

--- lib/shared/drawer.dart
+++ lib/shared/drawer.dart
@@ -5,43 +5,50 @@ import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 Drawer commonDrawer(BuildContext context) {
+  double width = MediaQuery.of(context).size.width;
+  double height = MediaQuery.of(context).size.height;
   return Drawer(
-    child: Center(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          FlatButton(
-            onPressed: () async {
-              SharedPreferences pref = await SharedPreferences.getInstance();
-              await pref.clear();
-              await FirebaseAuth.instance.signOut();
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => SignIn(),
-                ),
-              );
-            },
-            child: Text('Logout'),
-            color: Colors.red[300],
-          ),
-          FlatButton(
-            onPressed: () async {
-              SharedPreferences pref = await SharedPreferences.getInstance();
-              await pref.clear();
-              await FirebaseAuth.instance.signOut();
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => SignUp(),
-                ),
-              );
-            },
-            child: Text('Register'),
-            color: Colors.red[300],
-          ),
-        ],
-      ),
+    child: Column(
+      children: <Widget>[
+        DrawerHeader(
+          decoration: BoxDecoration(color: Color(0xff083EF6),),
+        child: Container(
+            width: width,
+            child: Align(
+                alignment: Alignment.bottomLeft,
+                child: Text("Welcome Back,",style: TextStyle(color: Colors.white,fontSize: 30,fontWeight: FontWeight.bold),))),),
+        SizedBox(height: height/4,),
+        FlatButton(
+          onPressed: () async {
+            SharedPreferences pref = await SharedPreferences.getInstance();
+            await pref.clear();
+            await FirebaseAuth.instance.signOut();
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (context) => SignIn(),
+              ),
+            );
+          },
+          child: Text('Logout',style: TextStyle(color: Colors.white)),
+          color: Color(0xff083EF6),
+        ),
+        FlatButton(
+          onPressed: () async {
+            SharedPreferences pref = await SharedPreferences.getInstance();
+            await pref.clear();
+            await FirebaseAuth.instance.signOut();
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (context) => SignUp(),
+              ),
+            );
+          },
+          child: Text('Register',style: TextStyle(color: Colors.white),),
+          color: Color(0xff083EF6),
+        ),
+      ],
     ),
   );
 }
